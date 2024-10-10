b7d3876dd34aeedb2d26e1e584bfa6d4c24dbb14
Igor Moura <igor.sousamoura@outlook.com>
2024-10-10 11:15:31 -0300

    Initializing project, classes made

---
+++ src/main/java/school/sptech/ex_many_to_one_dto1/exception/NaoEncontradoException.java
@@ -0,0 +1,6 @@
+package school.sptech.ex_many_to_one_dto1.exception;
+
+public class NaoEncontradoException {
+
+
+}
---
+++ src/main/java/school/sptech/ex_many_to_one_dto1/repository/CarteiraRepository.java
@@ -0,0 +1,5 @@
+package school.sptech.ex_many_to_one_dto1.repository;
+
+public interface CarteiraRepository {
+
+}
