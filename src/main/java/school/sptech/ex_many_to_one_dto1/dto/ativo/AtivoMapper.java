73548ecda4e6d3dd125dde5237403d26d03680e1
IgorMoura1 <igor.moura@sptech.school>
2024-10-15 23:43:12 -0300

    feat/ dto done, controllers still to finish

--- src/main/java/school/sptech/ex_many_to_one_dto1/dto/ativo/AtivoMapper.java
+++ src/main/java/school/sptech/ex_many_to_one_dto1/dto/ativo/AtivoMapper.java
@@ -1,6 +1,41 @@
 package school.sptech.ex_many_to_one_dto1.dto.ativo;
 
-// TODO: TERMINAR A CLASSE
+import school.sptech.ex_many_to_one_dto1.entity.Ativo;
+import school.sptech.ex_many_to_one_dto1.entity.Carteira;
+
 public class AtivoMapper {
 
+    public static AtivoResponseDto toAtivoResponseDto(Ativo ativo) {
+        if (ativo == null) return null;
+
+        Carteira carteira = ativo.getCarteira();
+
+        if (carteira == null) return null;
+
+        AtivoResponseDto.AtivoCarteiraResponseDto ativoCarteiraResponseDto =
+                AtivoResponseDto.AtivoCarteiraResponseDto
+                        .builder()
+                        .id(carteira.getId())
+                        .nome(carteira.getNome())
+                        .investidor(carteira.getInvestidor())
+                        .build();
+
+        return AtivoResponseDto
+                .builder()
+                .id(ativo.getId())
+                .nome(ativo.getNome())
+                .valorAtual(ativo.getValorAtual())
+                .carteira(ativoCarteiraResponseDto)
+                .build();
+    }
+
+    public static Ativo toAtivoEntity(AtivoRequestDto dto) {
+        if (dto == null) return null;
+
+        Ativo ativo = new Ativo();
+        ativo.setNome(dto.getNome());
+        ativo.setTipo(dto.getTipo());
+        ativo.setValorAtual(dto.getValorAtual());
+        return ativo;
+    }
 }
--- src/main/java/school/sptech/ex_many_to_one_dto1/service/AtivoService.java
+++ src/main/java/school/sptech/ex_many_to_one_dto1/service/AtivoService.java
@@ -1,6 +1,18 @@
 package school.sptech.ex_many_to_one_dto1.service;
 
-// TODO: TERMINAR A CLASSE
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.sptech.ex_many_to_one_dto1.entity.Ativo;
+import school.sptech.ex_many_to_one_dto1.repository.AtivoRepository;
+
+
+@Service
+@RequiredArgsConstructor
 public class AtivoService {
+
+    private final AtivoRepository ativoRepository;
+    private final CarteiraService carteiraService;
+
+
 
 }
